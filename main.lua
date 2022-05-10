69ad8a60339e50a6b7fb1dd67d9f2038a5934e12
tresks <55102988+tresks@users.noreply.github.com>
2022-05-10 21:17:15 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
+local LocalPlayer = game.Players.LocalPlayer;
+local MainGui = LocalPlayer.MainGui or LocalPlayer:WaitForChild("MainGui");
 local Notification = MainGui:WaitForChild("NotificationFrame");
 local NotificationSound = MainGui["Main Gui Core"].NotificationSound;
 function spawnNotification(p1, p2)
     local notification = Notification:Clone();
     notification.Parent = MainGui;
-    notification.NotificationText.Text = t;
+    notification.NotificationText.Text = p1;
     notification.Visible = true;
     NotificationSound:Play();
     if p2 then
@@ -13,4 +15,4 @@ function spawnNotification(p1, p2)
     end;
     notification.Visible = false;
     notification:Destroy();
-end
+end;
